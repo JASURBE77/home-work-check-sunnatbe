668f6e499cbcb6a854d97ccc9ef853068bf6615d
JASURBE77 <animesila89@gmail.com>
2025-12-24 14:08:05 +0500

    E

--- src/utils/api.js
+++ src/utils/api.js
@@ -6,24 +6,25 @@ export const instance = axios.create({
 });
 
 instance.interceptors.response.use(
-    (response) => response,
-    (error) => {
-        if (error.response) {
-            const status = error.response.status;
+  (response) => response,
+  (error) => {
+    if (error.response) {
+      const status = error.response.status;
 
-            if ([500, 502, 503, 429].includes(status)) {
-                AppRouter.push("/500")
-            }
+      if (status === 401) {
+        localStorage.removeItem("accessToken");
+        window.location.href = "/login";
+      }
 
-            if(status === 401) {
-                AppRouter.push("/login");
-            }
-
-        }
-
-        return Promise.reject(error);
+      if ([500, 502, 503, 429].includes(status)) {
+        window.location.href = "/500";
+      }
     }
-)
+
+    return Promise.reject(error);
+  }
+);
+
 
 export const api  = ({ url, open = false, ...props }) => {
     const token = localStorage.getItem("accessToken");
